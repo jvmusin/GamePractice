fb4a3599c74bf5702d752c4aa6a0129d36f9aaa3
AZ <anastasyazikina@gmail.com>
2016-05-25 01:20:05 +0400

    some refactoring

--- Battleship/GUI/MenueForm.cs
+++ Battleship/GUI/MenueForm.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Drawing;
 using System.Windows.Forms;
 
 namespace Battleship.GUI
@@ -7,7 +6,7 @@ namespace Battleship.GUI
     public class MenueForm : Form
     {
         private const int ButtonsCount = 1;
-        private const int SideFill =100;
+        private const int SideFill = 100;
 
         public MenueForm()
         {
@@ -20,18 +19,8 @@ namespace Battleship.GUI
             };
             startButton.Click += CreateBattleshipFiled;
 
-            var table = new TableLayoutPanel();
-
-            table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100));
-            table.RowStyles.Add(new RowStyle(SizeType.Absolute, SideFill));
-            table.RowStyles.Add(new RowStyle(SizeType.Percent, 100));
-            table.RowStyles.Add(new RowStyle(SizeType.Absolute, SideFill));
-            
-            table.Controls.Add(new Panel(), 0, 0);
-            table.Controls.Add(startButton, 0, 1);
-            table.Controls.Add(new Panel(), 0, 2);
-
-            table.Dock = DockStyle.Fill;
+            var buttonsTable = GetButtonsTableLayoutPanel(startButton);
+            var table = GetMainTableLayoutPanel(buttonsTable);
             Controls.Add(table);
         }
 
@@ -41,6 +30,46 @@ namespace Battleship.GUI
             var fieldForm = new CreateBattleshipFieldForm();
             fieldForm.Closed += (s, args) => Close();
             fieldForm.Show();
+        }
+
+        private TableLayoutPanel GetButtonsTableLayoutPanel(params Button[] buttons)
+        {
+            var table = new TableLayoutPanel();
+
+            table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100));
+
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, SideFill));
+            table.Controls.Add(new Panel());
+
+            foreach (var button in buttons)
+            {
+                table.RowStyles.Add(new RowStyle(SizeType.Percent, 100f / buttons.Length * 2));
+                table.Controls.Add(button);
+                table.RowStyles.Add(new RowStyle(SizeType.Percent, 100f / buttons.Length * 2));
+                table.Controls.Add(new Panel());
+            }
+
+            table.RowStyles.Add(new RowStyle(SizeType.Absolute, SideFill));
+
+            table.Dock = DockStyle.Fill;
+
+            return table;
+        }
+
+        private TableLayoutPanel GetMainTableLayoutPanel(TableLayoutPanel innerTable)
+        {
+            var table = new TableLayoutPanel();
+            table.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, SideFill));
+            table.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100));
+            table.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, SideFill));
+            
+
+            table.Controls.Add(new Panel(), 0, 0);
+            table.Controls.Add(innerTable, 1, 0);
+            table.Controls.Add(new Panel(), 2, 0);
+            table.Dock = DockStyle.Fill;
+
+            return table;
         }
     }
 }
